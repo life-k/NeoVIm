8842d5769761a1703ce8ba90aba533cc1738d73f
life-k <lifekgold@gmail.com>
2024-04-18 23:18:27 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.wo.number = true
+require("vim-options")
+require("lazy").setup("plugins")
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+-- Set indent width to 4 spaces
+vim.opt.shiftwidth = 4
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
