02219aa9affb8c2b47d678f873d059bb06584d18
life-k <lifekgold@gmail.com>
2024-07-06 18:34:22 +0300

    fixed plugins

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,11 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
-		vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, {})
-	end,
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    }
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,14 @@
 return {
 	{
 		"williamboman/mason.nvim",
+        lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+        lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "jdtls" },
@@ -15,6 +17,7 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+        lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
